14c1b2dea82df3d3442a420c975a664611229932
Raul Peterson <raul-luar2@hotmail.com>
2025-01-27 18:21:16 -0300

    FEAT: estrutura inicial

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Rauls on 27/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
